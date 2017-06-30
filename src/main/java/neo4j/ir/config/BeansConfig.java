669a1de75105f42425232c88c8f7fa6138720e52
Ali Asghar <taghizade12@gmail.com>
2017-06-30 09:30:57 +0430

    cleaned

--- src/main/java/neo4j/ir/config/BeansConfig.java
+++ src/main/java/neo4j/ir/config/BeansConfig.java
@@ -1,19 +1,12 @@
 package neo4j.ir.config;
 
-import iot.jcypher.database.DBAccessFactory;
-import iot.jcypher.database.DBProperties;
-import iot.jcypher.database.DBType;
-import iot.jcypher.database.IDBAccess;
 import neo4j.ir.Service.MyUserDetailService;
-import org.neo4j.driver.v1.AuthToken;
 import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import java.util.Properties;
 
 /**
  * Created by Ali Asghar on 27/06/2017.
@@ -25,23 +18,23 @@ public class BeansConfig {
     public UserDetailsService getUserDetailsService(){
         return new MyUserDetailService();
     }
-
-    @Bean
-    public IDBAccess idbAccess(){
-        Properties props = new Properties();
-
-        // properties for remote access and for embedded access
-        // (not needed for in memory access)
-        // have a look at the DBProperties interface
-        // the appropriate database access class will pick the properties it needs
-        props.setProperty(DBProperties.SERVER_ROOT_URI, "http://localhost:7474");
-        props.setProperty(DBProperties.DATABASE_DIR, "C:/NEO4J_DBS/01");
-
-        AuthToken authToken = AuthTokens.basic("neo4j", "neo");
-
-        /** connect to an in memory database (no properties are required) */
-        return DBAccessFactory.createDBAccess(DBType.REMOTE, props, authToken);
-    }
+//
+//    @Bean
+//    public IDBAccess idbAccess(){
+//        Properties props = new Properties();
+//
+//        // properties for remote access and for embedded access
+//        // (not needed for in memory access)
+//        // have a look at the DBProperties interface
+//        // the appropriate database access class will pick the properties it needs
+//        props.setProperty(DBProperties.SERVER_ROOT_URI, "http://localhost:7474");
+//        props.setProperty(DBProperties.DATABASE_DIR, "C:/NEO4J_DBS/01");
+//
+//        AuthToken authToken = AuthTokens.basic("neo4j", "neo");
+//
+//        /** connect to an in memory database (no properties are required) */
+//        return DBAccessFactory.createDBAccess(DBType.REMOTE, props, authToken);
+//    }
 
     @Bean
     public Driver driver(){
